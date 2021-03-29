89a6aea70ad7c3ca9de75c385b22eafe1c38cc6c
imranbenmassaoud <imranbenmassaoud@localhost>
2021-03-30 00:16:08 +0200

    SnakePanel (1)

--- SnakeGame/src/GameFrame.java
+++ SnakeGame/src/GameFrame.java
@@ -5,22 +5,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
+@SuppressWarnings({ "serial", "unused" })
 public class GameFrame extends JFrame {
 	
-	public static void main(String[] args)
-    {
+	public static void main(String[] args) {
+   
 		JFrame frame = new JFrame("SNAKE-VUB");
 		frame.setSize(800,800);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setVisible(true);
 	    frame.setResizable(false);
 	    frame.getContentPane().setBackground(Color.blue);
-	    
-		/*
-		 * color
-		 */
+	    JPanel hoofdpaneel = new SnakePanel();
+        frame.add(hoofdpaneel);
 	
     }
-	
-	
 }
--- SnakeGame/src/SnakePanel.java
+++ SnakeGame/src/SnakePanel.java
@@ -1,15 +1,75 @@
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
+
+@SuppressWarnings("serial")
 public class SnakePanel extends JPanel implements ActionListener {
+
+	JPanel hoofdpaneel = new JPanel();
+	hoofdpaneel.setBackground(Color.red);
+	hoofdpaneel.setBounds(0,0,50,50);
+	
+	
+    SnakePanel() {
+		
+	}
+	
+	public void StartGame() {
+		
+	}
+	
+	public void paintComponent (Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void Appels() {
+		
+	}
+	
+	public void Botsingen() {
+		
+	}
+	
+	public void GameOver(Graphics g) {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	public static void main(String[] args) {
+	   
+		JFrame frame = new JFrame("SNAKE-VUB");
+		frame.setSize(800,800);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		    
+	    frame.setVisible(true);
+		frame.setResizable(false);
+	    frame.getContentPane().setBackground(Color.blue);
+	    JPanel hoofdpaneel = new SnakePanel();	        
+	    frame.add(hoofdpaneel);
+		
+	    }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-
+		
+		
+	}
 }
